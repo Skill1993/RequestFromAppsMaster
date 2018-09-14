e79f3e3a72d55e0ebd8c70a41e1ccb7607a079bc
ZNA <skill1993@gmail.com>
2018-09-14 15:45:38 +0300

    1.0.0
    App creation with small amount of elements

---
+++ Mathematician/Mathematician/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Mathematician
+//
+//  Created by Никита Журавлев on 14.09.2018.
+//  Copyright © 2018 Никита Журавлев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
+
