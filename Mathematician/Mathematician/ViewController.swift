5cf205d3a39857e3a085e1371bd72e1f8f214cd1
ZNA <skill1993@gmail.com>
2018-09-14 16:57:48 +0300

    added random generic

--- Mathematician/Mathematician/ViewController.swift
+++ Mathematician/Mathematician/ViewController.swift
@@ -9,6 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var num1: Int = 0
+    var num2: Int = 0
+    var sum: Int = 0
+    var urAnswer: Int = 0
     
+    @IBOutlet weak var exmplLblb: UILabel!
+    @IBOutlet weak var urAns: UITextField!
+    
+    @IBAction func rfrshExmpl(_ sender: UIButton) {
+        num1 = Int.random(in: 0..<101)
+        num2 = Int.random(in: 0..<101)
+        sum = num1 + num2
+        
+        exmplLblb.text = ("\(num1) + \(num2)")
+    }
+    
+    @IBAction func answrTxt(_ sender: UITextField) {
+        urAnswer = Int(urAns.text ?? "0")!
+    }
+    
+    @IBAction func chckBtn(_ sender: UIButton) {
+        if urAnswer == sum{
+            let alertSuccess = UIAlertController(
+                title: "Resolution",
+                message: "You are correct",
+                preferredStyle: UIAlertController.Style.alert)
+            alertSuccess.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alertSuccess, animated: true, completion: nil)
+        } else {
+            let alertFail = UIAlertController(
+                title: "Resolution",
+                message: "You're not correct",
+                preferredStyle: UIAlertController.Style.alert)
+            alertFail.addAction(UIAlertAction(title: "Go back", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alertFail, animated: true, completion: nil)
+        }
+    }
 }
 
